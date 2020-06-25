c6d74d064fdfaff2ef60d50f785531edcfaf7fd1
Gernot Lenkner <g.lenkner@outlook.de>
2020-06-25 14:42:15 +0200

    + Test03 load data from json file and show list

---
+++ 03/app.js
@@ -0,0 +1,25 @@
+Vue.use(httpVueLoader);
+
+var vm = new Vue({
+    el: '#vue-app',
+    components: {
+        'my-component': 'url:my-component.vue',
+    },
+    data: {
+        title: 'Test 03',
+        data: {},
+        message: 'Loading...',
+    },
+    created: function() {
+        fetch('../assets/data/m.json')
+        .then(function(response) {
+            //console.log(response);
+            return response.json();
+        })
+        .then(function(data) {
+            //console.log(data);
+            vm.data = data;
+            vm.message = 'Loaded.';
+        });
+    },
+});
---
+++ 03/my-component.vue
@@ -0,0 +1,25 @@
+<template>
+          <ul>
+            <li v-for="item in artists" :key="item.value">
+              {{ item }}
+            </li>
+          </ul>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world',
+            artists: vm.data.artists,
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #fee;
+}
+</style>
+
